039336d7c31b02315a5025bc308160e545bfaa81
Aaron Kurowski <aaron12342@att.net>
2021-07-20 19:39:49 -0500

    Clothing model created and server.js configured.

--- api/models/clothingModel.js
+++ api/models/clothingModel.js
@@ -1,2 +1,41 @@
 // Import mongoose
 const mongoose = require("mongoose");
+
+// Declare schema and assign Schema class
+const Schema = mongoose.Schema;
+
+// Create Schema Instance and add schema properties
+const ClothingSchema = new Schema({
+    name: {
+        type: String, // Types: String, Number, Date, Buffer, Boolean, Mixed, ObjectId, Array, Decimal128, Map, Schema
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    color: {
+        type: String,
+        required: false
+    },
+    size: {
+        type: Number,
+        required: true
+    },
+    datePackaged: {
+        type: Date,
+        required: true,
+    },
+    condition: {
+        type: String,
+        required: false
+    },
+    outForDelivery: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+});
+
+// create and export model
+module.exports = mongoose.model("clothingModel", ClothingSchema);
---
+++ server.js
@@ -0,0 +1,22 @@
+// require express and bodyParser
+const express = require("express");
+const bodyParser = require("body-parser"); // parses incoming json
+
+const app = express(); // create express app
+
+// define port to run express app
+const port = process.env.PORT || 3000;
+
+// use bodyParser middleware on express app
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+// add endpoint
+app.get("/", (req, res) => {
+    res.send("Hello Universe!");
+});
+
+// listen to server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
